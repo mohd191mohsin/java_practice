40e2f8a2fdce54b7a784bfcf4d005a5ba9cfa6c7
mohd191mohsin <134185979+mohd191mohsin@users.noreply.github.com>
2026-04-30 22:23:23 +0530

    01 tut

---
+++ 01/Print/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(){
+        System.out.println("My name is Mohsin");
+        System.out.println("I am 26 years old");
+        System.out.println(2+3);
+    }
+}
